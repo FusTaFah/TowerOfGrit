0c4674b70003317865cf66c35a7f4675ed8be801
FusTaFah <mfadel_1990@hotmail.com>
2018-08-21 22:34:36 +0200

    updates PlayerControls.cs; final implementation of movement V1

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class PlayerControls : MonoBehaviour {
     public float    m_floatMovementSpeed;
     public float    m_floatJumpHeight;
+    public float    m_floatMaxSlope;
     float           m_floatXSpeed;
     GameObject      m_gameObjectAimLine;
     Vector3         m_vector3PlayerScale;
@@ -58,66 +59,83 @@ public class PlayerControls : MonoBehaviour {
         Vector2 boundingBoxBottomRight = boundingBoxCenter - new Vector2(-m_rigidBody2DplayerRig.GetComponent<Collider2D>().bounds.size.x / 2.0f + m_constFloatGroundDistanceQualifier, m_rigidBody2DplayerRig.GetComponent<Collider2D>().bounds.size.y / 2.0f + m_constFloatGroundDistanceQualifier);
         Collider2D rch = Physics2D.OverlapArea(boundingBoxBottomLeft, boundingBoxBottomRight);
 
-        if (rch != null)
-        {
-            if (moveX == 0.0f && !m_boolIsJumping)
-            {
-                m_rigidBody2DplayerRig.velocity = Vector2.zero;
-                m_floatXSpeed = 0.0f;
-            }
-            if (Input.GetKey(KeyCode.Joystick1Button0))
-            {
-                m_boolIsJumping = true;
-                m_rigidBody2DplayerRig.velocity = Vector2.zero;
-                m_rigidBody2DplayerRig.AddForce(new Vector2(0.0f, m_floatJumpHeight));
-            }
-        }
-        else
-        {
-            m_boolIsJumping = false;
-        }
-        Vector2 boundingBoxTopLeft = boundingBoxCenter - new Vector2(m_rigidBody2DplayerRig.GetComponent<Collider2D>().bounds.size.x / 2.0f + m_constFloatGroundDistanceQualifier * 2.0f, -m_rigidBody2DplayerRig.GetComponent<Collider2D>().bounds.size.y / 2.0f);
-        Vector2 boundingBoxTopRight = boundingBoxCenter - new Vector2(-m_rigidBody2DplayerRig.GetComponent<Collider2D>().bounds.size.x / 2.0f - m_constFloatGroundDistanceQualifier * 2.0f, -m_rigidBody2DplayerRig.GetComponent<Collider2D>().bounds.size.y / 2.0f);
+        
+        Vector2 boundingBoxTopLeft = boundingBoxCenter - new Vector2(m_rigidBody2DplayerRig.GetComponent<Collider2D>().bounds.size.x / 2.0f + m_constFloatGroundDistanceQualifier * 1.0f, -m_rigidBody2DplayerRig.GetComponent<Collider2D>().bounds.size.y / 2.0f);
+        Vector2 boundingBoxTopRight = boundingBoxCenter - new Vector2(-m_rigidBody2DplayerRig.GetComponent<Collider2D>().bounds.size.x / 2.0f - m_constFloatGroundDistanceQualifier * 1.0f, -m_rigidBody2DplayerRig.GetComponent<Collider2D>().bounds.size.y / 2.0f);
         RaycastHit2D slopeCheckLeft = Physics2D.Raycast(boundingBoxTopLeft, Vector2.down, m_rigidBody2DplayerRig.GetComponent<Collider2D>().bounds.size.y);
         RaycastHit2D slopeCheckRight = Physics2D.Raycast(boundingBoxTopRight, Vector2.down, m_rigidBody2DplayerRig.GetComponent<Collider2D>().bounds.size.y);
         Debug.DrawLine(boundingBoxTopLeft, boundingBoxTopLeft + Vector2.down * m_rigidBody2DplayerRig.GetComponent<Collider2D>().bounds.size.y, Color.green);
-        Debug.DrawLine(boundingBoxTopRight, boundingBoxTopRight + Vector2.down * m_rigidBody2DplayerRig.GetComponent<Collider2D>().bounds.size.y, Color.green);
+        Debug.DrawLine(boundingBoxTopRight, boundingBoxTopRight + Vector2.down * m_rigidBody2DplayerRig.GetComponent<Collider2D>().bounds.size.y, Color.red);
 
-        if (m_floatXSpeed <= -m_constFloatDeadZone)
+        bool enableJumping = true;
+        bool enableMovement = true;
+
+        if(slopeCheckLeft.collider != null || slopeCheckRight.collider != null)
         {
-            if(slopeCheckLeft.collider != null)
+            float heightFromFeetLeft = (slopeCheckLeft.collider == null ? 0.0f : m_rigidBody2DplayerRig.GetComponent<Collider2D>().bounds.size.y - slopeCheckLeft.distance);
+            float heightFromFeetRight = (slopeCheckRight.collider == null ? 0.0f : m_rigidBody2DplayerRig.GetComponent<Collider2D>().bounds.size.y - slopeCheckRight.distance);
+            m_debugUtil.AppendDebugger(heightFromFeetRight.ToString());
+            float angleBetweenLeft = Mathf.Atan(heightFromFeetLeft / m_constFloatGroundDistanceQualifier) * Mathf.Rad2Deg;
+            float angleBetweenRight = Mathf.Atan(heightFromFeetRight / m_constFloatGroundDistanceQualifier) * Mathf.Rad2Deg;
+
+            if (!m_boolFacingRight && angleBetweenLeft > m_floatMaxSlope && angleBetweenRight <= m_floatMaxSlope)
             {
-                float heightFromFeet = m_rigidBody2DplayerRig.GetComponent<Collider2D>().bounds.size.y - slopeCheckLeft.distance;
-                float angleBetween = Mathf.Atan(heightFromFeet / m_constFloatGroundDistanceQualifier) * Mathf.Rad2Deg;
-                m_debugUtil.AppendDebugger(angleBetween.ToString());
-                if (angleBetween < 45.0f)
+                enableJumping = false;
+                enableMovement = false;
+                m_debugUtil.AppendDebugger("00");
+            }
+            else if (!m_boolFacingRight && angleBetweenLeft > m_floatMaxSlope && angleBetweenRight > m_floatMaxSlope)
+            {
+                enableJumping = false;
+                enableMovement = true;
+                m_debugUtil.AppendDebugger("01");
+            }
+            else if (!m_boolFacingRight && angleBetweenLeft <= m_floatMaxSlope && angleBetweenRight > m_floatMaxSlope)
+            {
+                enableJumping = false;
+                enableMovement = true;
+                m_debugUtil.AppendDebugger("03");
+            }
+            else if (m_boolFacingRight && angleBetweenLeft > m_floatMaxSlope && angleBetweenRight > m_floatMaxSlope)
+            {
+                enableJumping = true;
+                enableMovement = false;
+                m_debugUtil.AppendDebugger("05");
+            }
+            else if (m_boolFacingRight && angleBetweenLeft <= m_floatMaxSlope && angleBetweenRight > m_floatMaxSlope)
+            {
+                enableJumping = false;
+                enableMovement = false;
+                m_debugUtil.AppendDebugger("07");
+            }
+
+        }
+
+        if (enableJumping)
+        {
+            if (rch != null)
+            {
+                if (moveX == 0.0f && !m_boolIsJumping)
                 {
-                    m_rigidBody2DplayerRig.velocity = new Vector2(m_floatXSpeed, m_rigidBody2DplayerRig.velocity.y);
+                    m_rigidBody2DplayerRig.velocity = Vector2.zero;
+                    m_floatXSpeed = 0.0f;
+                }
+                if (Input.GetKey(KeyCode.Joystick1Button0))
+                {
+                    m_boolIsJumping = true;
+                    m_rigidBody2DplayerRig.velocity = Vector2.zero;
+                    m_rigidBody2DplayerRig.AddForce(new Vector2(0.0f, m_floatJumpHeight));
                 }
             }
             else
             {
-                m_rigidBody2DplayerRig.velocity = new Vector2(m_floatXSpeed, m_rigidBody2DplayerRig.velocity.y);
+                m_boolIsJumping = false;
             }
         }
-        else if(m_floatXSpeed >= m_constFloatDeadZone)
+        if (enableMovement)
         {
-            if(slopeCheckRight.collider != null)
-            {
-                float heightFromFeet = m_rigidBody2DplayerRig.GetComponent<Collider2D>().bounds.size.y - slopeCheckRight.distance;
-                float angleBetween = Mathf.Atan(heightFromFeet / m_constFloatGroundDistanceQualifier) * Mathf.Rad2Deg;
-                m_debugUtil.AppendDebugger(angleBetween.ToString());
-                if (angleBetween < 45.0f)
-                {
-                    m_rigidBody2DplayerRig.velocity = new Vector2(m_floatXSpeed, m_rigidBody2DplayerRig.velocity.y);
-                }
-            }
-            else
-            {
-                m_rigidBody2DplayerRig.velocity = new Vector2(m_floatXSpeed, m_rigidBody2DplayerRig.velocity.y);
-            }
+            m_rigidBody2DplayerRig.velocity = new Vector2(m_floatXSpeed * (m_boolIsCrouching ? 0.5f : 1.0f), m_rigidBody2DplayerRig.velocity.y);
         }
-        
 
         float moveY = Input.GetAxis("Vertical");
         if (moveY < -0.5f)
@@ -126,9 +144,29 @@ public class PlayerControls : MonoBehaviour {
         }
         else
         {
-            m_boolIsCrouching = false;
+            if (m_boolIsCrouching)
+            {
+                Vector2 centreOfCrouched = m_rigidBody2DplayerRig.GetComponent<Collider2D>().bounds.center;
+                float yOffsetToFullHeight = m_rigidBody2DplayerRig.GetComponent<Collider2D>().bounds.size.y;
+                yOffsetToFullHeight *= 0.5f;
+                yOffsetToFullHeight *= 3.0f;
+                Vector2 standUpHeight = new Vector2(centreOfCrouched.x, centreOfCrouched.y + yOffsetToFullHeight + 0.1f);
+                Vector2 topLeft = standUpHeight - new Vector2(m_rigidBody2DplayerRig.GetComponent<Collider2D>().bounds.size.x / 2.0f, 0.0f);
+                Vector2 topRight = standUpHeight + new Vector2(m_rigidBody2DplayerRig.GetComponent<Collider2D>().bounds.size.x / 2.0f, 0.0f);
+                Collider2D crouchTopLine = Physics2D.OverlapArea(topLeft, topRight);
+                Debug.DrawLine(topLeft, topRight, Color.blue);
+
+                if (crouchTopLine == null)
+                {
+                    m_boolIsCrouching = false;
+                }
+                else
+                {
+                    m_boolIsCrouching = true;
+                }
+            }
+
         }
-        m_debugUtil.AppendDebugger("is jumping? " + m_boolIsJumping);
     }
 
     private void ResolveAimLine()
@@ -157,6 +195,7 @@ public class PlayerControls : MonoBehaviour {
     private void ResolveFacingDirection()
     {
         m_vector3PlayerScale = new Vector3((m_boolFacingRight ? 1 : -1) * Mathf.Abs(m_vector3PlayerScale.x), m_vector3PlayerScale.y, m_vector3PlayerScale.z);
+        
         gameObject.transform.localScale = new Vector3(m_vector3PlayerScale.x, m_vector3PlayerScale.y / (m_boolIsCrouching ? 2.0f : 1.0f), m_vector3PlayerScale.z);
     }
 }
